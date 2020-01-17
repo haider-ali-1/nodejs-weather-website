b9c7fae1cbb2095ea63cb263ed2b4164b2024994
haider-ali-1 <haider.ali.developer123@gmail.com>
2020-01-17 13:15:55 +0500

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
     temp.textContent = ''
 
-    fetch(`http://localhost:3000/weather?address=${search.value}`).then((response) => {
+    fetch(`/weather?address=${search.value}`).then((response) => {
     response.json().then((data) => {
         
         if (data.error) {
